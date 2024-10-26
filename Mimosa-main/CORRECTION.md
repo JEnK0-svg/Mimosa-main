c78d1cf3c96ef7bdbfe1054e3ee085e565450475
Jenk <954096499@qq.com>
2024-10-26 14:43:19 +0800

    bigru

---
+++ Mimosa-main/CORRECTION.md
@@ -0,0 +1,9 @@
+## Correction Statement for Algorithm S1
+In the supplementary Algorithm S1 of our paper, the gap penalty value should be 1. <br> <br>
+
+![image](https://github.com/user-attachments/assets/a628af85-df13-4929-8f89-605ea17e8bf7)
+ <br> <br> <br>
+
+
+
+
---
+++ Mimosa-main/training/MJnet.py
@@ -0,0 +1,386 @@
+import pandas as pd
+import numpy as np
+import sys,os, re
+import torch.nn as nn
+import torch.optim as optim
+import torch
+from tqdm import tqdm
+
+from sklearn.metrics import recall_score,f1_score
+from sklearn.metrics import accuracy_score,average_precision_score
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+from utils import *
+
+torch.manual_seed(417)
+torch.set_num_threads(20)
+
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+class myDataset(Dataset):
+
+    def __init__(self, data):
+        self.data = data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self,index):
+        self.sample = self.data[index]
+        self.mirna, self.mrna, self.label = self.sample
+        self.reverse_mrna = reverse_seq(self.mrna)
+        self.mirna = self.mirna + 'X' * (26 - len(self.mirna))
+        onehot_m = get_onehot_embedding(self.reverse_mrna)
+        onehot_mi = get_onehot_embedding(self.mirna)
+        
+        pairing_m, pairing_mi = get_interaction_map(self.mirna,self.reverse_mrna)
+
+        onehot_m  = torch.tensor(onehot_m, dtype=torch.float32).to(device)
+        onehot_mi = torch.tensor(onehot_mi, dtype=torch.float32).to(device)
+        pairing_m = torch.tensor(pairing_m, dtype=torch.float32).to(device)
+        pairing_mi = torch.tensor(pairing_mi, dtype=torch.float32).to(device)
+
+        label = torch.tensor(self.label, dtype=torch.float32).to(device)
+        
+        return {'fea1': onehot_m,
+                'fea2': onehot_mi,
+                'fea3': pairing_m,
+                'fea4': pairing_mi,
+                'label': label,
+               }
+
+class MJnet(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_heads, dropout, output_size):
+        super(MJnet, self).__init__()
+        
+        # 使用双向GRU（输入维度加上pairing特征，共5维）
+        self.gru_m = nn.GRU(input_size, hidden_size, num_layers, dropout=dropout, bidirectional=True, batch_first=True)
+        self.gru_mi = nn.GRU(input_size, hidden_size, num_layers, dropout=dropout, bidirectional=True, batch_first=True)
+
+        # Cross attention mechanism (hidden_size * 2 due to bidirectional GRU)
+        self.cross_attention = nn.MultiheadAttention(hidden_size * 2, num_heads, dropout=dropout)
+
+        # Fully connected layers for final classification
+        self.fc1 = nn.Linear(40, 16)
+        self.dropout = nn.Dropout(dropout)
+        self.batch_norm1 = nn.BatchNorm1d(16)
+        self.fc2 = nn.Linear(16, output_size)
+
+    def forward(self, onehot_m, onehot_mi, pairing_m, pairing_mi):
+        # 将pairing特征与one-hot编码拼接，形成5维输入 (4维one-hot + 1维pairing)
+        m_input = torch.cat((onehot_m, pairing_m.unsqueeze(-1)), dim=-1)  # (batch_size, seq_len, 5)
+        mi_input = torch.cat((onehot_mi, pairing_mi.unsqueeze(-1)), dim=-1)
+
+        # Bi-directional GRU Encoder
+        m_emb, _ = self.gru_m(m_input)  # Output shape: (batch_size, seq_len, 2 * hidden_size)
+        mi_emb, _ = self.gru_mi(mi_input)
+
+        # Permute for multihead attention
+        m_emb = m_emb.permute(1, 0, 2)  # (seq_len, batch_size, 2 * hidden_size)
+        mi_emb = mi_emb.permute(1, 0, 2)
+
+        # Cross attention mechanism
+        cross_attend, _ = self.cross_attention(m_emb, mi_emb, mi_emb)
+
+        # Mean pooling after attention, followed by fully connected layers
+        output = cross_attend.permute(1, 0, 2).mean(dim=2)
+
+        # Classification
+        output = self.fc1(output)
+        output = self.dropout(output)
+        output = torch.relu(output)
+        output = self.batch_norm1(output)
+        output = self.fc2(output)
+
+        return output
+
+# define the model architecture of Mimosa
+class Transformer(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_heads, dropout, output_size):
+        super(Transformer, self).__init__()
+
+        self.embedding_m = nn.Embedding(input_size, hidden_size)
+        self.position_encoding_m = nn.Parameter(torch.zeros(1, 100, hidden_size))
+        self.interaction_embedding_m = nn.Embedding(3, hidden_size)
+        nn.init.normal_(self.position_encoding_m, mean=0, std=0.1)
+
+
+        self.embedding_mi = nn.Embedding(input_size, hidden_size)
+        self.position_encoding_mi = nn.Parameter(torch.zeros(1, 100, hidden_size))
+        self.interaction_embedding_mi = nn.Embedding(3, hidden_size)
+        nn.init.normal_(self.position_encoding_mi, mean=0, std=0.1)
+
+
+        encoder_layers_m = nn.TransformerEncoderLayer(hidden_size, num_heads, hidden_size, dropout)
+        self.encoder_m = nn.TransformerEncoder(encoder_layers_m, num_layers)
+
+        encoder_layers_mi = nn.TransformerEncoderLayer(hidden_size, num_heads, hidden_size, dropout)
+        self.encoder_mi = nn.TransformerEncoder(encoder_layers_mi, num_layers)
+
+        self.cross_attention = nn.MultiheadAttention(hidden_size, num_heads)
+
+        self.fc1 = nn.Linear(40, 12)
+        self.fc2 = nn.Linear(12, output_size)
+
+
+    def forward(self, emb_m, emb_mi, pairing_m, pairing_mi):
+
+
+        m_emb = self.embedding_m(emb_m) + self.position_encoding_m[:, :emb_m.size(1), :] + self.interaction_embedding_m(pairing_m)
+        mi_emb = self.embedding_mi(emb_mi)  + self.position_encoding_mi[:,:emb_mi.size(1),:] + self.interaction_embedding_mi(pairing_mi)
+
+        m_emb = m_emb.permute(1, 0, 2)
+        mi_emb = mi_emb.permute(1, 0, 2)
+
+        encoder_output_m = self.encoder_m(m_emb)
+        encoder_output_mi = self.encoder_mi(mi_emb)
+
+        cross_attend, _ = self.cross_attention(encoder_output_m, encoder_output_mi, encoder_output_mi)
+        output = cross_attend.permute(1,0,2).mean(dim=2)
+
+
+        output = self.fc1(output)
+        output = torch.relu(output)
+        output = self.fc2(output)
+        out = torch.softmax(output, dim=1)
+        return out
+
+
+def Deep_train(model, dataloader, optimizer, criterion):
+
+    model.train()
+    counter = 0
+    train_loss = 0.0
+
+    for i, data in enumerate(tqdm(dataloader, desc="Training", leave=False)):
+        counter += 1
+        features1, features2, features3, features4, target = data['fea1'], data['fea2'], data['fea3'], data['fea4'], data['label']
+        
+        # 将特征和标签移动到与模型相同的设备
+        features1 = features1.to(device)
+        features2 = features2.to(device)
+        features3 = features3.to(device)
+        features4 = features4.to(device)
+        target = target.to(device)
+
+        target = target.unsqueeze(1)
+        outputs = model(features1,features2,features3,features4).to(device)
+        loss = criterion(outputs,target)
+        train_loss += loss.item()
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    train_avg_loss = train_loss/counter
+    return train_avg_loss
+
+
+def Deep_validate(model, dataloader, criterion):
+    model.eval()
+    counter = 0
+    val_loss = 0.0
+    all_predictions = []
+    all_targets = []
+    with torch.no_grad():
+        for i, data in enumerate(tqdm(dataloader, desc="Validating", leave=False)):
+            counter += 1
+            features1, features2, features3, features4, target = data['fea1'], data['fea2'], data['fea3'], data['fea4'],data['label']
+            
+            # 将特征和标签移动到与模型相同的设备
+            features1 = features1.to(device)
+            features2 = features2.to(device)
+            features3 = features3.to(device)
+            features4 = features4.to(device)
+            target = target.to(device)
+            target = target.unsqueeze(1)
+
+            outputs = model(features1, features2,features3,features4).to(device)
+            loss = criterion(outputs,target)
+
+            val_loss += loss.item()
+            predictions = []
+            outputs = outputs.cpu().numpy()
+            target = target.cpu().numpy()
+            # print(type(outputs))
+            for i in outputs:
+                if i > 0.5:
+                    predictions.append(1)
+                else:
+                    predictions.append(0)
+            all_predictions.extend(predictions)
+            all_targets.extend([i for i in target])
+
+        val_total_loss = val_loss/counter
+
+        acc = accuracy_score(all_targets, all_predictions)
+        pre = average_precision_score(all_targets, all_predictions)
+        recall = recall_score(all_targets,all_predictions)
+        spec = specificity_score(all_targets,all_predictions)
+        f1 = f1_score(all_targets,all_predictions)
+        npv = NPV(all_targets, all_predictions)
+
+
+        print('acc',acc)
+        print('pre',pre)
+        print('recall',recall)
+        print('specificity',spec)
+        print('f1',f1)
+        print('npv',npv)
+        
+
+        return val_total_loss
+
+
+def perform_train(filepath):
+    # train positive: 26995, train negative: 27469, val positive: 2193, val negative: 2136
+    batchsize = 128
+    learningrate = 1e-4
+    epochs = 40
+    train, val = read_data(filepath)
+
+    train_dataset = myDataset(train)
+    val_dataset = myDataset(val)
+    train_loader = DataLoader(train_dataset,batch_size=batchsize,shuffle=True) #collate_fn=my_collate_fn,
+    val_loader = DataLoader(val_dataset, batch_size=batchsize,shuffle=True) #,collate_fn=my_collate_fn
+    
+
+    # model = Transformer(input_size=5, hidden_size=64, num_layers=16, num_heads=8, dropout=0.1, output_size=2).to(device)
+    model = MJnet(input_size=5, hidden_size=128, num_layers=2, num_heads=8, dropout=0.1, output_size=1).to(device)
+    # criterion = nn.CrossEntropyLoss()
+    criterion = nn.BCEWithLogitsLoss()
+
+    optimizer = optim.Adam(model.parameters(), lr=learningrate,weight_decay=1e-5) #,weight_decay=1e-5
+
+    best_val_loss = 1
+    train_loss = []
+    valid_loss = []
+    for epoch in range(epochs):
+        print(f'Epoch {epoch+1} of {epochs}')
+        train_epoch_loss = Deep_train(model, train_loader,optimizer, criterion)
+        valid_epoch_loss = Deep_validate(model,val_loader,criterion)
+
+        train_loss.append(train_epoch_loss)
+        valid_loss.append(valid_epoch_loss)
+        print('Train Loss:', train_epoch_loss)
+        print('Val Loss:',valid_epoch_loss)
+        if valid_epoch_loss < best_val_loss:
+            best_val_loss = valid_epoch_loss
+            torch.save(model,'model_concate_{}.pth'.format(epoch))
+
+
+
+
+def get_cts(rmrna, stepsize):
+    '''segment full-length mRNAS into 40-nt segments using a sliding window with predefined stepsize'''
+    kmers = []
+
+    if len(rmrna) >= 40:
+        for i in range(0, len(rmrna),stepsize):
+            if i + 40 <= len(rmrna):
+                cut = rmrna[i:i + 40]
+                kmers.append(cut)
+
+        return kmers
+    else:
+        pad_rmrna = rmrna + 'X' * (40 - len(rmrna))
+        kmers.append(pad_rmrna)
+        return kmers
+
+
+
+
+def kmers_predict(kmers,mirna,model):
+
+    mirna = mirna + 'X'*(26-len(mirna))
+    fea1 = []
+    fea2 = []
+    fea3 = []
+    fea4 = []
+    if len(kmers) == 0:
+        return 0
+    else:
+        for i in kmers:
+            fea_1 = get_embedding(i)
+            fea_2 = get_embedding(mirna)
+            if 'X' in i:
+                pairing_m, pairing_mi = get_interaction_map_for_test_short(mirna, i)
+            else:
+                pairing_m, pairing_mi = get_interaction_map_for_test(mirna, i)
+
+            fea1.append(fea_1)
+            fea2.append(fea_2)
+            fea3.append(pairing_m)
+            fea4.append(pairing_mi)
+
+
+        fea1 = torch.tensor(fea1, dtype=torch.long).to(device)
+        fea2 = torch.tensor(fea2).to(device)
+        fea3 = torch.tensor(fea3).to(device)
+        fea4 = torch.tensor(fea4).to(device)
+
+        model = model.to(device)
+        pros = model(fea1, fea2, fea3, fea4).detach().cpu().numpy().tolist()
+        pppp = decision_for_whole(pros)
+
+        return pppp
+
+
+
+
+
+def perform_test(pathfile,stepsize):
+
+    test = read_test(pathfile)
+    y_true = []
+    y_pred = []
+
+    model = torch.load('model_mimosa.pth')
+    model = model.to(device)
+    print('个数',len(test))
+
+    for index in range(len(test)): #range(len(test))
+        fasta = test[index]
+
+
+        mirna = fasta[0].upper().replace('T', 'U')
+
+        mrna = fasta[1].upper().replace('T', 'U')
+        reverse_mrna = reverse_seq(mrna)
+        y_true.append(fasta[2])
+
+
+        kmers = get_cts(reverse_mrna,stepsize)
+
+        if kmers is None:
+            pre = 0
+            y_pred.append(pre)
+
+        else:
+
+            pre = kmers_predict(kmers, mirna, model)
+            y_pred.append(pre)
+
+    print(y_true)
+    print(y_pred)
+    acc = accuracy_score(y_true, y_pred)
+    pre = average_precision_score(y_true, y_pred)
+    recall = recall_score(y_true, y_pred)
+    spec = specificity_score(y_true, y_pred)
+    f1 = f1_score(y_true, y_pred)
+    auc = NPV(y_true, y_pred)
+
+    print('acc', acc)
+    print('PPV', pre)
+    print('recall', recall)
+    print('specificity', spec)
+    print('f1', f1)
+    print('NPV', auc)
+
+
+
+# path = '/your/path/to/Mimosa'
+# train_dataset_path = path + '/Data/miRAW_Train_Validation.txt'
+# test_dataset_path = path + '/Data/miRAW_Test0.txt'
+# perform_train(train_dataset_path)
+# perform_test(test_dataset_path, stepsize=1)
